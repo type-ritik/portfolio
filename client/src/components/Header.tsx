9e4699b70e0cb73cef5fe752fa1707d282e9b30e
rksvw <ritik.vw98@gmail.com>
2025-10-30 20:19:33 +0530

    header component is created

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,17 +1,18 @@
-const navigation =["Home", "About", "Project", "Contact"]
+const navigation: Array<string> = ["home", "about", "project", "contact"]
+const logo: string = "PORTFOLIO";
 
 function Header() {
   return (
-    <div className="flex">
-        <div>
-        <h2>Portfolio</h2>
+    <div className="flex justify-between items-center py-6! h-[120px] font-bold">
+        <div className="block">
+        <h2 className="text-xl">{logo.toUpperCase()}</h2>
         </div>
-        <div>
+        <div className="w-2/6 flex justify-between text-xl">
             {
                 navigation.map((item, index) => (
-                    <a key={index}>
+                    <a key={index} href={"\\" + item.toLowerCase()}>
                         <span>
-                            {item}
+                            {item.toLocaleLowerCase()}
                         </span>
                     </a>
                 ))
