689010f87ddcd1f16fbdb74f761da683f1be5ee7
rksvw <ritik.vw98@gmail.com>
2026-02-03 08:25:52 +0530

    project container responsiveness

--- client/src/components/ProjectContainer.tsx
+++ client/src/components/ProjectContainer.tsx
@@ -4,46 +4,46 @@ function ProjectContainer() {
       <h1>Projects Section</h1>
       <div className="w-full flex bg-[#a7d0fc] p-4! rounded-sm justify-between my-6!">
         <div className="flex flex-col gap-4">
-          <h2>
+          <h2 className="not-md:text-3xl!">
             Project 1: ChatSys - Real-time Chat System{" "}
-            <span>
+            <span className="not-md:text-2xl!">
               [
               <a
                 href="https://type-ritik-chat-sys.onrender.com/"
-                className="text-blue-600 mx-5!"
+                className="text-blue-600 mx-5! not-md:text-xl"
               >
                 LIVE
               </a>
               ]
             </span>
           </h2>
-          <p className="text-md! bg-gray-50 p-2! rounded">
+          <p className="text-md! not-md:text-base! bg-gray-50 p-2! rounded">
             A production-deployed real-time chat system build to understand
             service separation, state management, and backend reliability.
           </p>
           <h3>Architecture:</h3>
           <ul>
-            <li>Backend service (API + Auth)</li>
-            <li>Client application</li>
-            <li>PostgreSQL database</li>
-            <li>Redis for caching / real-time coordination</li>
-            <li>All services deployed independently on Render</li>
+            <li className="m-1! p-1! text-md!">Backend service (API + Auth)</li>
+            <li className="m-1! p-1! text-md!">Client application</li>
+            <li className="m-1! p-1! text-md!">PostgreSQL database</li>
+            <li className="m-1! p-1! text-md!">Redis for caching / real-time coordination</li>
+            <li className="m-1! p-1! text-md!">All services deployed independently on Render</li>
           </ul>
           <h3>Key Engineering Decisions:</h3>
           <ul>
-            <li>Chose service separation to avoid tight coupling</li>
-            <li>Used Redis to handle shared state and reduce DB load</li>
-            <li>
+            <li className="m-1! p-1! text-md!">Chose service separation to avoid tight coupling</li>
+            <li className="m-1! p-1! text-md!">Used Redis to handle shared state and reduce DB load</li>
+            <li className="m-1! p-1! text-md!">
               Implemented JWT-based authentication to protect private messages
             </li>
           </ul>
           <h3>Challenges & Learnings</h3>
           <ul>
-            <li>Encountered issues that only appeared after deployment</li>
-            <li>
+            <li className="m-1! p-1! text-md!">Encountered issues that only appeared after deployment</li>
+            <li className="m-1! p-1! text-md!">
               Learned how environment configuration impacts system behavior
             </li>
-            <li>
+            <li className="m-1! p-1! text-md!">
               Understood why production debugging is different from local
               development
             </li>
