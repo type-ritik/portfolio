8c044a58ae680722776f41d558549a4bd6c623f5
rksvw <ritik.vw98@gmail.com>
2026-02-03 08:17:16 +0530

    skill container responsive design mobile friendly

--- client/src/components/SkillContainer.tsx
+++ client/src/components/SkillContainer.tsx
@@ -2,42 +2,42 @@ function SkillContainer() {
   return (
     <>
         <h1>Skills</h1>
-      <div className="w-full flex bg-[#a7d0fc] p-4! rounded-sm justify-between my-6!">
+      <div className="w-full flex bg-[#a7d0fc] p-4! not-md:flex-col not-md:gap-6! rounded-sm justify-between my-6!">
         <div>
-          <h3>Backend Engineering</h3>
+          <h3 className="m-4!">Backend Engineering</h3>
           <ul>
-            <li>
+            <li className="m-1! p-1! text-md!">
               Designed REST and GraphQL APIs with authentication and role-based
               access
             </li>
-            <li>
+            <li className="m-1! p-1! text-md!">
               Modeled real-world entities and invariants to maintain data
               integrity
             </li>
           </ul>
           <br />
-          <h3>System Design & Deployment</h3>
+          <h3 className="m-4!">System Design & Deployment</h3>
           <ul>
-            <li>
+            <li className="m-1! p-1! text-md!">
               Deployed independently running backend, client, database, and
               Redis services
             </li>
-            <li>
+            <li className="m-1! p-1! text-md!">
               Managed environment variables, service networking, and production
               failures
             </li>
           </ul>
           <br />
-          <h3>Data & State Management</h3>
+          <h3 className="m-4!">Data & State Management</h3>
           <ul>
-            <li>PostgreSQL schema design with relational integrity</li>
-            <li>Redis for caching and real-time coordination</li>
+            <li className="m-1! p-1! text-md!">PostgreSQL schema design with relational integrity</li>
+            <li className="m-1! p-1! text-md!">Redis for caching and real-time coordination</li>
           </ul>
-          <h3>Engineering Practices</h3>
+          <h3 className="m-4!">Engineering Practices</h3>
           <ul>
-            <li>JWT-based authentication and authorization</li>
-            <li>Debugging deployment-only failures</li>
-            <li>Version control with Git and structured commits</li>
+            <li className="m-1! p-1! text-md!">JWT-based authentication and authorization</li>
+            <li className="m-1! p-1! text-md!">Debugging deployment-only failures</li>
+            <li className="m-1! p-1! text-md!">Version control with Git and structured commits</li>
           </ul>
         </div>
         <div className="rounded-sm">
